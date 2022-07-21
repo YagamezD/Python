f252e64109e2c3a77259ff4194abef5453b459c4
YagamezD <yanisagamez@gmail.com>
2022-07-21 02:01:09 -0500

    Python

--- CuentaBancaria/main.py
+++ CuentaBancaria/main.py
@@ -6,12 +6,12 @@ lola = Usuario("Lola")
 
 
 print("----JUANA---")
-# Haz que el 1er usuario haga 3 depositos y 1 giro, luego muestre balance
+# Haz que el 1er usuario haga 3 depositos, 1 retiro, genera intereses y muestra la informacion de la cuenta 
 juana.mostrar_balance()
 juana.hacer_deposito(15).hacer_deposito(15).hacer_deposito(200).hacer_retiro(100).generar_interes().mostrar_balance()
 
 print("---LOLA----")
-# Haz que el 2do usuario haga 1 deposito y 3 giros, luego muestre balance
+# Haz que el 2do usuario haga 2 deposito, 4 retiros, genera intereses y muestra la informacion de la cuenta 
 lola.mostrar_balance()
 lola.hacer_deposito(180).hacer_deposito(200).hacer_retiro(20).hacer_retiro(30).hacer_retiro(10).hacer_retiro(20).generar_interes().mostrar_balance()
 
